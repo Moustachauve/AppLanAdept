2fbc5f91d21a705f2e8340bfda7d0b339afca948
Christophe Gagnier <admin@cgagnier.ca>
2015-09-11 16:44:29 -0400

    Uncomplete things

--- app/src/main/java/ca/cgagnier/lanadept/services/IUserService.java
+++ app/src/main/java/ca/cgagnier/lanadept/services/IUserService.java
@@ -1,4 +1,12 @@
 package ca.cgagnier.lanadept.services;
 
+import ca.cgagnier.lanadept.models.User;
+
 public interface IUserService {
+
+    public User tryLogin(String username, String password);
+
+    public User subscribe(String username, String password, String passwordConfirmation, String fullName);
+
+    public
 }
