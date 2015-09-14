a04c07332a8182b54bb8be5b701016207829e801
Christophe Gagnier <admin@cgagnier.ca>
2015-09-14 17:15:50 -0400

    Debut interface

--- app/src/main/java/ca/cgagnier/lanadept/MainActivity.java
+++ app/src/main/java/ca/cgagnier/lanadept/MainActivity.java
@@ -1,10 +1,14 @@
 package ca.cgagnier.lanadept;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.joda.time.DateTime;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTimer(new DateTime(2015, 10, 14, 12, 0));
+
     }
 ;
     @Override
@@ -35,5 +42,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setTimer(DateTime startingDate) {
+
+
+
+        CountDownTimer mCountDownTimer;
+        long mInitialTime = Math.abs(startingDate.getMillis() - DateTime.now().getMillis());
+
+        mCountDownTimer = new CountDownTimer(mInitialTime, 1000) {
+            StringBuilder time = new StringBuilder();
+            @Override
+            public void onFinish() {
+                //mTextView.setText(DateUtils.formatElapsedTime(0));
+                //mTextView.setText("Times Up!");
+            }
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                time.setLength(0);
+                // Use days if appropriate
+                if(millisUntilFinished > DateUtils.DAY_IN_MILLIS) {
+                    long count = millisUntilFinished / DateUtils.DAY_IN_MILLIS;
+                    if(count > 1)
+                        time.append(count).append(" days ");
+                    else
+                        time.append(count).append(" day ");
+
+                    millisUntilFinished %= DateUtils.DAY_IN_MILLIS;
+                }
+
+                time.append(DateUtils.formatElapsedTime(Math.round(millisUntilFinished / 1000d)));
+                //mTextView.setText(time.toString());
+            }
+        }.start();
     }
 }
